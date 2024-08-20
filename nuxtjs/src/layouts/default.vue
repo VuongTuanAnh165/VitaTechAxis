6e0130744ebd0dfcad5270b057c3f471e881f8a1
VuongTuanAnh165 <92902120+VuongTuanAnh165@users.noreply.github.com>
2024-08-20 21:27:46 +0700

    commit 1769 2

--- nuxtjs/src/layouts/default.vue
+++ nuxtjs/src/layouts/default.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Capacitor } from '@capacitor/core'
 
 const footer = ref<{
   readonly height: number
@@ -9,17 +8,6 @@ const FooterContentMargin = 32
 const footerHeight = computed(() => {
   return footer.value?.height ?? 0
 })
-
-const platform = Capacitor.getPlatform()
-
-const { checkUpdateApp } = useAppChange()
-
-const callAppAddListeners = async () => {
-  if (platform !== 'web') {
-    await checkUpdateApp()
-  }
-}
-// await callAppAddListeners()
 
 const storeVersion = useVersionStore()
 const { updateApp } = storeToRefs(storeVersion)
